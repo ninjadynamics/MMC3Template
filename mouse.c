7ceb89ca09e7ce4b53d7bc8abaa64c2db39e64bc
Ninja Dynamics <brunosilva.work@gmail.com>
2024-08-21 15:08:58 +0200

    Optimize mouse code

--- mouse.c
+++ mouse.c
@@ -1,5 +1,4 @@
 #include "mouse.h"
-
 #include <peekpoke.h>
 
 #define MOUSE_PORT 0x4016
@@ -13,14 +12,10 @@ bool mouse_right;
 
 #pragma bss-name (push, "ZEROPAGE")
 static uint8_t new_x, new_y;
-static uint8_t first_byte;
-static uint8_t second_byte;
-static uint8_t third_byte;
-static uint8_t fourth_byte;
 static int8_t x_velocity;
 static int8_t y_velocity;
-static uint32_t report;
-static uint8_t i, bit;
+static uint8_t report[4];
+static uint8_t i, t, bit;
 #pragma bss-name (pop)
 
 void __fastcall__ init_mouse(uint8_t x, uint8_t y) {
@@ -33,35 +28,29 @@ bool __fastcall__ read_mouse(void) {
   POKE(MOUSE_PORT, 1);
   POKE(MOUSE_PORT, 0);
 
-  // Read 32 bits from the mouse
+  // Read 32 bits from the mouse into the report array
   for (i = 0; i < 32; ++i) {
-    bit = PEEK(MOUSE_PORT) & 0x01;
-    report <<= 1;
-    report |= bit;
+    // Shift and insert bit into the correct byte
+    bit = PEEK(MOUSE_PORT) & 0x01; t = i >> 3;
+    report[t] = (report[t] << 1) | bit;
   }
 
-  // Parse the data into respective bytes
-  first_byte = (report >> 24) & 0xFF;
-  second_byte = (report >> 16) & 0xFF;
-  third_byte = (report >> 8) & 0xFF;
-  fourth_byte = report & 0xFF;
-
   // Check if the mouse is connected
-  if ((second_byte & 0x0F) != 0x01) return false;
+  if ((report[1] & 0x0F) != 0x01) return false;
 
   // Extract button states
-  mouse_left = (second_byte >> 6) & 0x01;
-  mouse_right = (second_byte >> 7) & 0x01;
+  mouse_left = (report[1] >> 6) & 0x01;
+  mouse_right = (report[1] >> 7) & 0x01;
 
   // Convert vertical displacement to two's complement
-  y_velocity = third_byte & 0x7F;
-  if (third_byte & 0x80) {
+  y_velocity = report[2] & 0x7F;
+  if (report[2] & 0x80) {
     y_velocity = -y_velocity;
   }
 
   // Convert horizontal displacement to two's complement
-  x_velocity = fourth_byte & 0x7F;
-  if (fourth_byte & 0x80) {
+  x_velocity = report[3] & 0x7F;
+  if (report[3] & 0x80) {
     x_velocity = -x_velocity;
   }
 
